ce4e1ebb91cf5f716997020ac8c0c473e5b61e78
George Barnick <georgebarnick@gmail.com>
2016-03-04 01:15:03 -0500

    Convert to switch-based ad code

--- showAdSenseAds.php
+++ showAdSenseAds.php
@@ -1,35 +1,40 @@
 <style type="text/css">
 html, body {
         margin: 0;
-        paddin: 0;
+        padding: 0;
 }
 </style>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <?php
-if ($_GET['size'] == "250") {
-        echo '<!-- Square 250 -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:250px;height:250px"
-             data-ad-client="ca-pub-9393485036030091"
-             data-ad-slot="2497640960"></ins>';
-} else if ($_GET['size'] == "728") {
-        echo '<!-- Leaderboard Ad -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:728px;height:90px"
-             data-ad-client="ca-pub-9393485036030091"
-             data-ad-slot="9488014163"></ins>';
-} else if ($_GET['size'] == "320") {
-        echo '<!-- Mobile 320 Ad -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:320px;height:100px"
-             data-ad-client="ca-pub-9393485036030091"
-             data-ad-slot="4824678565"></ins>';
-} else {
-        echo '<ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-9393485036030091"
-             data-ad-slot="7756942160"
-             data-ad-format="auto"></ins>';
+switch ( $_GET['size'] ) {
+        case "250":
+                echo '<!-- Square 250 -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:250px;height:250px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="2497640960"></ins>';
+                break;
+        case "728":
+                echo '<!-- Leaderboard Ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:728px;height:90px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="9488014163"></ins>';
+                break;
+        case "320":
+                echo '<!-- Mobile 320 Ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:320px;height:100px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="4824678565"></ins>';
+                break;
+        default:
+                echo '<ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="7756942160"
+                     data-ad-format="auto"></ins>';
+                break;
 }
 ?>
 <script>
