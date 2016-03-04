7fcaaad5745cd204b55053db31f37353cf9e48fa
George Barnick <georgebarnick@gmail.com>
2016-03-04 01:31:30 -0500

    Simplify usage.sh

--- usage.sh
+++ usage.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+dd="/var/www/html/owncloud/data/" # data directory path
+
 while test $# -gt 0
 do
         case "$1" in
-                -a) for d in $(sudo find /var/www/html/owncloud/data/ -maxdepth 1 -type d)
+                -a) for d in $(sudo find $dd -maxdepth 1 -type d)
                 do
-                        if [[ ( $d != /var/www/html/owncloud/data/updater_backup ) && ( $d != /var/www/html/owncloud/data/demo ) && ( $d != /var/www/html/owncloud/data/ ) && ( $d != /var/www/html/owncloud/data/admin ) ]]; then
+                        if [[ ( $d != ${dd}updater_backup ) && ( $d != ${dd}demo ) && ( $d != ${dd}admin ) && ( $d != $dd ) ]]; then
                                 sudo du -hs $d;
                         fi
                 done
                 ;;
                 -*) echo "Error: Invalid argument."
                 ;;
-                *) sudo du -hs /var/www/html/owncloud/data/$1
+                *) sudo du -hs $dd$1
         esac
         shift
 done
